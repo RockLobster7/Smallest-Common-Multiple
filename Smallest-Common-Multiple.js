19692dee3708377bb6169e3eff7ee1f87d1c0b83
Mario Cueva <mario.cueva.au@gmail.com>
2017-09-29 16:17:57 +1000

    completed challenge

--- Smallest-Common-Multiple.js
+++ Smallest-Common-Multiple.js
@@ -7,12 +7,46 @@ The range will be an array of two numbers that will not necessarily be in numeri
 e.g. for 1 and 3 - find the smallest common multiple of both 1 and 3 that is evenly divisible by all numbers between 1 and 3.
 */
 
+
 function smallestCommons(arr) {
-    return arr;
+
+  var max = Math.max(arr[0], arr[1]);
+  var min = Math.min(arr[0], arr[1]);
+
+  //build an array containing all numbers between min and max
+  var numRange = [];
+  for (let i = min; max >= i; i++) {
+    numRange.push(i);
   }
 
-  console.log(smallestCommons([1, 5])); // should return a number.
-  console.log(smallestCommons([1, 5])); // should return 60.
-  console.log(smallestCommons([5, 1])); // should return 60.
-  console.log(smallestCommons([1, 13])); // should return 360360.
-  console.log(smallestCommons([23, 18])); // should return 6056820.
+  // process each number in range
+  var multiple = 1;
+  var result = min;
+  numRange.map(function (val) {
+    console.log("current val: " + val);
+    //loop until the minimum multiple is divisible by the current range value
+    while (min % val !== 0) {
+
+      multiple = multiple + 1;
+      min = multiple * result;
+      // console.log("min:" + min + " % " + "val:" + val + " =" + min % val);
+    }
+
+    //store the result for the current divible range value
+    result = min;
+    // console.log("current result: " + result);
+
+    //reset the multiple and do the next value in the range
+    multiple = 1;
+    return;
+  });
+
+  return result;
+
+}
+
+console.log(smallestCommons([1, 5])); // should return a number.
+console.log(smallestCommons([1, 5])); // should return 60.
+console.log(smallestCommons([5, 1])); // should return 60.
+console.log(smallestCommons([1, 13])); // should return 360360.
+console.log(smallestCommons([23, 18])); // should return 6056820.
